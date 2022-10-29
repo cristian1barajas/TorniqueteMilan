51085cf401a9a4c9378240c5655be681d9adcaa4
SISTRAN\cbarajas <cbarajas@sistran.com.co>
2022-10-29 16:28:51 -0500

    Versión con petición web

--- src/config.h
+++ src/config.h
@@ -1,10 +1,26 @@
 #include <Arduino.h>
 #include <UIPEthernet.h>
 
+#define LED_ETHERNET_CONNECTED 26
+#define LED_ETHERNET_DISCONNECTED 25
+
+#define INPUT_SOLENOID_RELAY 27
+#define OUTPUT_SOLENOID_RELAY 12
+
+#define INPUT_INDICATOR 14
+#define OUTPUT_INDICATOR 13
+
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-byte ipServer[] = {192, 168, 1, 90};
-const byte portRemote = 80;
-byte ipLocal[] = {192, 168, 1, 91};
+byte ipServer[] = {192, 168, 0, 1};
+const int portRemote = 9081;
+byte ipLocal[] = {192, 168, 0, 90};
+
+String preUrl = "GET /api/v1/registrovisitas/visitantes/validarAcceso?codigoBuscar=";
+String postUrl = "&estacion=P-03&lugar=5";
+String idNumber = "1031124702";
 
 void ethernetShieldConnection(void);
 void ethernetCableConnection(void);
+void digitalConfiguration(void);
+void testSequence(void);
+void sendWebRequest(void);
